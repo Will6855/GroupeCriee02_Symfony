2eab664a45c5077854199e615aac4f72f46ebd0f
Will <86987398+Will6855@users.noreply.github.com>
2023-11-27 15:53:41 +0100

    Initial Commit

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+    #[Route('/factures', name: 'factures')]
+    public function factures(): Response
+    {
+        return $this->render('accueil/factures.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+    #[Route('/lots', name: 'lots')]
+    public function lots(): Response
+    {
+        return $this->render('accueil/lots.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
